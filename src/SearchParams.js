107c753bf28b32e40c3fd1a2d7a0f17bae46aab6
Moki <mokiong1427@gmail.com>
2021-10-08 16:08:09 +0800

    Create animal dropdown list

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,13 @@
 import { useState } from 'react';
 
+const ANIMALS = ['Bird', 'cat', 'dog', 'rabbit', 'reptile'];
+
 const SearchParams = () => {
    const [location, setLocation] = useState('Seattle, WA');
+   const [animal, setAnimal] = useState('');
+   const [breed, setBreed] = useState('');
+
+   const breeds = [];
 
    return (
       <div className="search-params">
@@ -14,6 +20,38 @@ const SearchParams = () => {
                   value={location}
                   placeholder="location"
                />
+            </label>
+            <label htmlFor="animal">
+               Animal
+               <select
+                  id="animal"
+                  value={animal}
+                  onChange={(e) => setAnimal(e.target.value)}
+                  onBlur={(e) => setAnimal(e.target.value)}
+               >
+                  <option />
+                  {ANIMALS.map((animal) => (
+                     <option key={animal} value={animal}>
+                        {animal}asdasd
+                     </option>
+                  ))}
+               </select>
+            </label>
+            <label htmlFor="breed">
+               Breed
+               <select
+                  id="breed"
+                  value={animal}
+                  onChange={(e) => setBreed(e.target.value)}
+                  onBlur={(e) => setBreed(e.target.value)}
+               >
+                  <option />
+                  {breeds.map((breed) => (
+                     <option key={breed} value={breed}>
+                        {breed}
+                     </option>
+                  ))}
+               </select>
             </label>
             <button>Submit</button>
          </form>
