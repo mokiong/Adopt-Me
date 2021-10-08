838c20296a46db90631cc3376c79eb0aa62dff00
Moki <mokiong1427@gmail.com>
2021-10-08 18:43:33 +0800

    Make carousel

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import Carousel from './Carousel';
 
 class Details extends Component {
    state = { loading: true };
@@ -22,9 +23,12 @@ class Details extends Component {
    }
 
    render() {
-      const { animal, breed, city, state, description, name } = this.state;
+      const { animal, breed, city, state, description, name, images } =
+         this.state;
+      console.log(images);
       return (
          <div className="details">
+            <Carousel images={images} />
             <div>
                <h1>{name}</h1>
                <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
