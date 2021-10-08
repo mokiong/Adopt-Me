7069f9e759b968278dabc2262309430050df449d
Moki <mokiong1427@gmail.com>
2021-10-08 15:30:26 +0800

    Convert to jsx

--- src/app.js
+++ src/app.js
@@ -1,17 +1,24 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Pet from './Pet';
 
+// const App = () => {
+//    return React.createElement('div', {}, [
+//       React.createElement('h1', {}, 'Adopt Me!'),
+//       React.createElement(Pet, {
+//          name: 'Luna',
+//          animal: 'dog',
+//          breed: 'havanese',
+//       }),
+//    ]);
+// };
 const App = () => {
-   return React.createElement('div', {}, [
-      React.createElement('h1', {}, 'Adopt Me!'),
-      React.createElement(Pet, {
-         name: 'Luna',
-         animal: 'dog',
-         breed: 'havanese',
-      }),
-   ]);
+   return (
+      <div>
+         <h1>Adopt Me!</h1>
+         <Pet name="Luna" animal="Dog" breed="Havanse" />
+      </div>
+   );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
